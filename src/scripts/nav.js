107ccf6ff8e20ec7cf246be614caaefcd32130f7
tgarijo <tgarijo@gmail.com>
2020-02-10 07:10:06 +0100

    commit

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,33 +1,93 @@
-/*const navMenu = document.getElementById("navMenu")
-const logo = `<img src="../img/logo.png">`
-const navString = 
-` 
-  <img src="../img/logo.png">
-  <nav>
-    <a class="link" href="./home.html" title="Home">Home</a>
-    <a class="link" href="./books.html" title="Libros">Libros</a>
-    <a class="link" href="./listBook.html" title="Listado Libros">Listado Libros</a>
-    <a class="link" href="./listOfBorrowed.html" title="Listado Libros Presados">Listado Libros Presados</a>
-    <a class="link" href="./soporte.html" title="Soporte">Soporte</a>
-    <a class="link" href="#" title="Salir">Salir</a>
-  </nav>
- `
-//navMenu.innerHTML = logo;
-navMenu.innerHTML = navString;
-*/
-
-
-/*
-var link = document.querySelector('link[rel="import"]');
-console.log(link);
-
-var template = link.import.querySelector('#fragment');
-var clone = document.importNode(template.content, true);
-
-document.querySelector('#header').appendChild(clone);
-
-*/
+var error=false;
 
 $(function(){
   $("#header").load("./headerPartial.html"); 
+  //$(".formBook input").value="";
 });
+
+var main = function() {
+   
+    $(".formBook button").on("click", function(event){
+    
+      // I use an object I avoid set an if for each 
+      // inputs,  on error controls
+      var data = {
+        "title" : $("#title"),
+        "author" : $("#author"),
+        "editorial": $("#editorial"),
+        "isbn": $("#isbn"),
+        "available": $("#available")
+      }
+    
+      // checking if all data set on inputs forms
+      // Object.keys(data).forEach(function(key) 
+      //   {checkField(data[key]);});
+      
+      if (!error) composeLines(data)
+      
+
+    })
+
+    $(".formBook input").on("keypress", function(event){
+      
+      // set color on inputs if it has an error
+      if(error) {
+        $(event.currentTarget).css("background-color", "white")
+        $(".errors").empty(); 
+      }
+    })
+}
+
+var composeLines = function(data) {
+  var linesOfBooks = `<span> ${data['title'][0].value} </span>`
+  linesOfBooks += `<span> ${data['author'][0].value} </span>`
+  linesOfBooks += `<span> ${data['editorial'][0].value} </span>`
+  linesOfBooks += `<span> ${data['isbn'][0].value} </span>`
+  linesOfBooks += `<span> ${data['available'][0].value} </span>`
+
+  $("#detail").append(linesOfBooks);    
+}
+// function for check if inputs are void
+var checkField = function ($control){
+    var $errorMassage = "Debe rellenar campo"
+    //console.log($control[0].value)
+    if($control[0].value==="") {
+      error = true;
+      errors($errorMassage, $control);
+    } else error = false;
+    
+    return error
+}
+
+// function show errors on div
+var errors = function($textError, $control) {
+  $(".errors").empty(); 
+  var $errors = $(".errors"); 
+
+  $(".errors").append($textError); 
+  $($control[0]).css("background-color", "pink")
+  
+}
+
+$(document).ready(main);
+
+/*
+var addData = function() {
+
+  "use strict";
+
+  var data= [];
+
+  var form = document.getElementById('formBook');
+  var college = form.elements.titulo.value;
+  var autor = form.elements.autor.value;
+  var editorial = form.elements.editorial.value;
+  var isbn = form.elements.isbn.value;
+  
+  var divDetail = document.getElementById('detail');
+
+  var node = '<span>'+ college + '</span>';
+
+  divDetail.innerHTML += node;
+}
+*/
